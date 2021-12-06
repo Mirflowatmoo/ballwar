7a7719105c454ba4ecc81717637927837a584f91
3304300253@qq.com <3304300253@qq.com>
2021-12-06 19:50:35 +0800

    ..

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//branch master
+
 #include <iostream>
 #include <Windows.h>
 #include <graphics.h>
@@ -14,9 +16,12 @@ constexpr int MOVESPEED = 3;
 constexpr int FOODNUM = 10;
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 760;
+constexpr int MAP_WIDTH = 5000;
+constexpr int MAP_HEIGHT = 5000;
 constexpr int PLAYER_POSX = 640;
 constexpr int PLAYER_POSY = 380;
-int posxdif = 0, posydif = 0, radius = 20;
+int g_posxdif = 0, g_posydif = 0, g_playerRadius = 20;
+int g_playerPosxOnMap = 0, g_playerPosyOnMap = 0;
 
 typedef struct tagFoodBall {
 	int posx;
@@ -39,25 +44,25 @@ int main() {
 	srand(unsigned(time(NULL)));
 	initgraph(1280, 760, 1);
 	setbkcolor(WHITE);
-	
+
 	FoodBall* pFoodBall = GameInitFoodBall();
 
 	BeginBatchDraw();
 	while (1) {
 		DWORD beginTime = GetTickCount();
 		//input return area
-		posxdif = posydif = 0;
+		g_posxdif = g_posydif = 0;
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-			posxdif = MOVESPEED;
+			g_posxdif = MOVESPEED;
 		}
 		if (GetAsyncKeyState(VK_UP) & 0x8000) {
-			posydif = MOVESPEED;
+			g_posydif = MOVESPEED;
 		}
 		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-			posxdif = -MOVESPEED;
+			g_posxdif = -MOVESPEED;
 		}
 		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-			posydif = -MOVESPEED;
+			g_posydif = -MOVESPEED;
 		}
 
 		//state changing area
@@ -66,11 +71,11 @@ int main() {
 		//collision detection area
 		EatFood(pFoodBall, FOODNUM);
 
-		
+
 
 		ShowFood(pFoodBall, FOODNUM);
 		setfillcolor(RED);
-		solidcircle(PLAYER_POSX, PLAYER_POSY, radius);
+		solidcircle(PLAYER_POSX, PLAYER_POSY, g_playerRadius);
 
 		FlushBatchDraw();
 
@@ -97,13 +102,13 @@ void ShowFood(FoodBall* foodBall, int n) {
 
 void MoveFood(FoodBall* foodBall, int n) {
 	for (int index = 0; index < n; ++index) {
-		foodBall[index].posx += (rand() % 5 - 2 + posxdif);
-		foodBall[index].posy += (rand() % 5 - 2 + posydif);
+		foodBall[index].posx += (rand() % 5 - 2 + g_posxdif);
+		foodBall[index].posy += (rand() % 5 - 2 + g_posydif);
 	}
 }
 
 void EatFood(FoodBall* foodBall, int n) {
-	int judgeValue = radius >> 1;
+	int judgeValue = g_playerRadius >> 1;
 
 	for (int index = 0; index < n; ++index) {
 		int fposx = foodBall[index].posx;
@@ -112,12 +117,12 @@ void EatFood(FoodBall* foodBall, int n) {
 
 
 		if (abs(PLAYER_POSX - fposx) <= judgeValue && abs(PLAYER_POSY - fposy) <= judgeValue) {
-			if (radius < fradius) {
+			if (g_playerRadius < fradius) {
 				continue;
 			}
-			radius += (fradius >> 2);
-			foodBall[index].posx = rand() % 1280;
-			foodBall[index].posy = rand() % 760;
+			g_playerRadius += (fradius >> 2);
+			foodBall[index].posx = rand() % 2560;
+			foodBall[index].posy = rand() % 1520;
 			foodBall[index].radius = rand() % 15 + 5;
 		}
 	}
@@ -127,7 +132,7 @@ FoodBall* GameInitFoodBall() {
 
 	FoodBall* pFoodBall = nullptr;
 	pFoodBall = new FoodBall[FOODNUM];
-	
+
 	for (int index = 0; index < FOODNUM; ++index) {
 		pFoodBall[index] = { rand() % 1280, rand() % 760, rand() % 15 + 5, RGB(rand() % 255, rand() % 255, rand() % 255), FOODSHOW };
 	}
