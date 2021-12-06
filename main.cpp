59fd4a6638ab7ab659083f8ce4795a262432bce8
3304300253@qq.com <3304300253@qq.com>
2021-12-06 21:38:35 +0800

    ..

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //branch master
-
+//555
 #include <iostream>
 #include <Windows.h>
 #include <graphics.h>
@@ -10,26 +10,37 @@
 using std::cout;
 using std::endl;
 
-constexpr int FOODSHOW = 0;
-constexpr int FOODHIDE = 1;
-constexpr int MOVESPEED = 3;
-constexpr int FOODNUM = 10;
-constexpr int SCREEN_WIDTH = 1280;
-constexpr int SCREEN_HEIGHT = 760;
-constexpr int MAP_WIDTH = 5000;
-constexpr int MAP_HEIGHT = 5000;
-constexpr int PLAYER_POSX = 640;
-constexpr int PLAYER_POSY = 380;
-int g_posxdif = 0, g_posydif = 0, g_playerRadius = 20;
-int g_playerPosxOnMap = 0, g_playerPosyOnMap = 0;
+constexpr int FOODSHOW = 0; //食物球显示标记
+constexpr int FOODHIDE = 1; //食物球隐藏标记
+constexpr int ENEMY_ALIVE = 0; //敌球生存标记
+constexpr int ENEMY_DEAD = 1; //敌球死亡标记
+constexpr int SCREEN_WIDTH = 1280; //窗口宽度
+constexpr int SCREEN_HEIGHT = 760; //窗口高度
+constexpr int MAP_WIDTH = 5000; //地图宽度
+constexpr int MAP_HEIGHT = 5000; //地图高度
+constexpr int PLAYER_POSX = 640; //玩家在窗口客户区的横坐标（默认为中间）
+constexpr int PLAYER_POSY = 380; //玩家在窗口客户区的纵坐标（默认为中间）
+constexpr int ENEMY_BALL_NUM = 10; //敌球的数量
+
+int g_posxdif = 0, g_posydif = 0; //全局对象坐标偏移量
+int g_playerRadius = 20; //玩家球体半径
+int g_playerPosxOnMap = 2500, g_playerPosyOnMap = 2500; //玩家球体位于地图的坐标
+int g_moveSpeed = 3; //移动速度
+int g_foodBallNum = 10; //食物球的数量
 
 typedef struct tagFoodBall {
-	int posx;
-	int posy;
+	int posx, posy;
 	int radius;
 	COLORREF color;
 	int flag;
 } FoodBall, * PtrFoodBall;
+
+typedef struct tagEnemyBall {
+	int posx, posy;
+	int radius;
+	COLORREF color;
+	int flag;
+} EnemyBall, * PtrEnemyBall;
 
 void ShowFood(FoodBall* foodBall, int n);
 
@@ -53,27 +64,26 @@ int main() {
 		//input return area
 		g_posxdif = g_posydif = 0;
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-			g_posxdif = MOVESPEED;
+			g_posxdif = g_moveSpeed;
 		}
 		if (GetAsyncKeyState(VK_UP) & 0x8000) {
-			g_posydif = MOVESPEED;
+			g_posydif = g_moveSpeed;
 		}
 		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-			g_posxdif = -MOVESPEED;
+			g_posxdif = -g_moveSpeed;
 		}
 		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-			g_posydif = -MOVESPEED;
+			g_posydif = -g_moveSpeed;
 		}
 
 		//state changing area
-		MoveFood(pFoodBall, FOODNUM);
+		MoveFood(pFoodBall, g_foodBallNum);
 
 		//collision detection area
-		EatFood(pFoodBall, FOODNUM);
+		EatFood(pFoodBall, g_foodBallNum);
 
 
-
-		ShowFood(pFoodBall, FOODNUM);
+		ShowFood(pFoodBall, g_foodBallNum);
 		setfillcolor(RED);
 		solidcircle(PLAYER_POSX, PLAYER_POSY, g_playerRadius);
 
@@ -131,9 +141,9 @@ void EatFood(FoodBall* foodBall, int n) {
 FoodBall* GameInitFoodBall() {
 
 	FoodBall* pFoodBall = nullptr;
-	pFoodBall = new FoodBall[FOODNUM];
+	pFoodBall = new FoodBall[g_foodBallNum];
 
-	for (int index = 0; index < FOODNUM; ++index) {
+	for (int index = 0; index < g_foodBallNum; ++index) {
 		pFoodBall[index] = { rand() % 1280, rand() % 760, rand() % 15 + 5, RGB(rand() % 255, rand() % 255, rand() % 255), FOODSHOW };
 	}
 
